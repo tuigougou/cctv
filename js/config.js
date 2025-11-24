051236e15bb0c95fc965e812474ada7371167381
tuigougou <95177623+tuigougou@users.noreply.github.com>
2025-11-25 00:25:31 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -13,9 +13,9 @@ const PASSWORD_CONFIG = {
 
 // 网站信息配置
 const SITE_CONFIG = {
-    name: 'LibreTV',
-    url: 'https://libretv.is-an.org',
-    description: '免费在线视频搜索与观看平台',
+    name: '啾啾TV',
+    url: 'https://cctv-csu.pages.dev/',
+    description: '啾啾之家',
     logo: 'image/logo.png',
     version: '1.0.3'
 };
